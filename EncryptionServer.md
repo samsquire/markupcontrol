48061b4ecd867d02457484c2ac82a57f3d5f58dc
Sam Squire <ssquire@thoughtworks.com>
2013-01-13 22:50:38 +0000

    Process subnodes too.

---
+++ EncryptionServer.md
@@ -0,0 +1,16 @@
+<document> <sam name="header.md"/>
+<sam type="sparql" name="encserver.sparql">
+<sam name="idea.prefix"/>	
+INSERT DATA {
+:SamServer a owl:Class; a :Idea;
+	 dc:title "Sam Server";
+	 dc:description "Sam's idea";
+	 dc:identifier "SamServer".
+}
+</sam>
+<![CDATA[
+<div class="content SamServer">
+This page was mostly from Ideatable.
+</div>
+]]><sam name="fulltable.md"/>
+</document>
--- join.sh
+++ join.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 FILE="$1"
+MERGING="$2"
 
 if [[ -e $FILE ]]; then
 	cp $FILE ./merged/
-	FILE="./merged/$FILE"
+	FILE="./merged/$(basename $FILE)"
 	echo "Using copied $FILE"
 	# Replace each file with output
 	while read -r line;
@@ -13,6 +14,7 @@ if [[ -e $FILE ]]; then
 		TYPE=$(xml sel -t -v "//sam[@name = '$INPUT']/@type" "./sections/$INPUT") 	
 		if [[ -z "$TYPE" ]]; then
 				NESTED=$(xml sel -t -v "count(//sam[@name = '$INPUT']/child::sam)" "./sections/$INPUT")	
+				FILENESTED=$(xml sel -t -v "count(//sam[@name = '$INPUT']/child::sam)" "$FILE")	
 				if [[ "$NESTED" -gt 0 ]]; then
 				echo "Untyped has $NESTED nested"
 						echo "Untyped has $NESTED nested"
@@ -22,6 +24,16 @@ if [[ -e $FILE ]]; then
 						# TEXT=$(xml sel -t -v "/document//sam[@name = '$INPUT']" "$FILE" | xml unesc)
 						TEXT=$(xml sel -t -v "/sam[@name = '$INPUT']" "./sections/$INPUT" | xml unesc)
 				fi	
+				# The source file has no nested elements, only the section does.
+				if [[ "$FILENESTED" = 0 ]]; then
+					if [[ -z "$MERGING" ]]; then
+						OK=1
+						./join.sh "./sections/$INPUT" "merging"
+						echo "Source file has no nested children. Must be completely sourced."
+						TEXT=$(<./merged/$INPUT)
+						echo $TEXT
+					fi
+				fi
 				echo "Untyped text $INPUT"
 			else
 				echo "Typed text $INPUT"	
@@ -29,13 +41,13 @@ if [[ -e $FILE ]]; then
 		fi
 	
 		if [[ "$OK" == 1 ]]; then
-			xml ed -L --insert "/document//sam[@name = '$INPUT']" --type text --name ignored --value "$TEXT" "$FILE"
+			xml ed -L --insert "//sam[@name = '$INPUT']" --type text --name ignored --value "$TEXT" "$FILE"
 			# Delete the old node
-			xml ed -L --delete "/document//sam[@name = '$INPUT']" "$FILE"
+			xml ed -L --delete "//sam[@name = '$INPUT']" "$FILE"
 		fi
 
-	done < <(xml sel -t -v '/document//sam/@name' "$FILE")
-	xml sel -t -v '/document' $FILE > $FILE.tmp
+	done < <(xml sel -t -v '//sam/@name' "$FILE")
+	xml sel -t -v '/' $FILE > $FILE.tmp
 	xml unesc - < $FILE.tmp > $FILE
 	rm $FILE.tmp
 	else
