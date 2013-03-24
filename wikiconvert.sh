a7594d08a98428166a0f1646f3833e9e6e1b2e27
Sam Squire <ssquire@thoughtworks.com>
2013-03-24 18:39:28 +0000

    Document wikidpad converter. <Sam/>

--- wikiconvert.sh
+++ wikiconvert.sh
@@ -1,5 +1,9 @@
 FILE="$1"
 
+# WikiLinks to Markdown links
+# WikiLink names to .html links
+# WikidPad headings to Markdown headings
+# Replace multiple spaces with one
 gsed -e 's@^[A-Z]\([A-Z0-9]*[a-z][a-z0-9]*[A-Z]\|[a-z0-9]*[A-Z][A-Z0-9]*[a-z]\)[A-Za-z0-9]*@[\0]@g' \
  	-e 's@\[\(.*\)\]\($\|\s\)@[\1](\1.html)\2@g' \
 	-e 's@+@#@g' \
