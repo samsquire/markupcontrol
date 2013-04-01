da139e19d731a2d65869b69d77109653d439bb97
Sam Squire <ssquire@thoughtworks.com>
2013-04-01 19:58:21 +0100

    Fixed wikiwords. <Sam>

--- wikiconvert.sh
+++ wikiconvert.sh
@@ -4,7 +4,8 @@ FILE="$1"
 # WikiLink names to .html links
 # WikidPad headings to Markdown headings
 # Replace multiple spaces with one
-gsed -e 's@^[A-Z]\([A-Z0-9]*[a-z][a-z0-9]*[A-Z]\|[a-z0-9]*[A-Z][A-Z0-9]*[a-z]\)[A-Za-z0-9]*@[\0]@g' \
- 	-e 's@\[\(.*\)\]\($\|\s\)@[\1](\1.html)\2@g' \
-	-e 's@+@#@g' \
- 	-e 's@^\s*\(.*\)\*@\* \1@g' "$FILE"
+gsed -r -e 's@[A-Z]([A-Z0-9]*[a-z][a-z0-9]*[A-Z]|[a-z0-9]*[A-Z][A-Z0-9]*[a-z])[A-Za-z0-9]*@[\0](\0.html)@g' \
+	-e 's@\[\(.*\)\]\($\|\s\)@[\0](\0.html)\0@g' \
+	-e 's@\+@#@g' \
+  -e 's@^\s*\*@ *@g' "$FILE"
+
