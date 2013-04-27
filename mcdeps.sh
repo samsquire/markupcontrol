46cb932c737cde346a41ce4b5a083b711b1a5d98
Sam Squire <ssquire@thoughtworks.com>
2013-04-27 10:03:55 +0100

    Trying to get the wikilink functionality to not conflict with my XML and markdown. <Sam>

--- mcdeps.sh
+++ mcdeps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FILE="$1"
+NOPREFIX=${2:-0}
 SECTIONLINES=""
 DEPLINES=""
 
@@ -16,11 +17,27 @@ do
 	fi
 	DEPLINES="$DEPLINES ${LOCATION}.d"	
 	SECTIONLINES="$SECTIONLINES ${LOCATION}"	
-done < <(xml unesc - < "$FILE" | xml sel -t -v "//sam//@name")
+	echo "$LOCATION: ${FILE}.outputs"
+done < <(xml unesc - < "$FILE" | xml sel -t -v '//sam[text() != ""]/@name')
+
+while read -r line;
+do
+	echo "./merged/${FILE}: ./sections/$line"
+done < <(xml unesc - < "$FILE" | xml sel -t -v '//sam[not(text())]/@name')
+
+if [ $NOPREFIX == "1" ]; then
+	echo $SECTIONLINES
+	exit 0
+fi
 
 # echo "${FILE}:$DEPLINES"
-if [[ $FILE != "idea.md" ]]; then
-		echo "${FILE}: idea.md $SECTIONLINES"
+if [[ $FILE == "ideatable" ]]; then
+		echo "./merged/${FILE}: $SECTIONLINES"
 	else
-		echo "${FILE}: $SECTIONLINES"
+		echo "./merged/${FILE}: ./merged/header.md ./merged/fulltable.md"
+		echo "./merged/${FILE}: idea.md ${FILE}.outputs $SECTIONLINES"
 fi
+
+echo "${FILE}.outputs: ${FILE}"
+echo "	extractfile.sh ${FILE}"
+echo "	touch ${FILE}.outputs"
